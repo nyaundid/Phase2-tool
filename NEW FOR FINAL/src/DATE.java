5c9fe15896ae3bad49b09f001cb3849e5cce6840
seis-602-fall2017 <darius.nyaundi@LAPTOP-QG5H3RPR>
2017-12-09 23:14:39 -0600

    new

--- NEW FOR FINAL/src/DATE.java
+++ NEW FOR FINAL/src/DATE.java
@@ -2,15 +2,17 @@ public class DATE
 
 {
 
-	public static void main(String[] args)
+	public static void main(String[] args) {
 
-	{
 		java.util.Date date = new java.util.Date();
 		System.out.println(
-				
+				"the dates and times for the elapsedtimes 10,100,1000,10000,100000,1000000,10000000,100000000");
+
+		for (long i = 10000; i <= 1e11; i *= 20) {
 			date.setTime(i);
+
 			System.out.println(date.toString());
-			System.out.println("Elapsed Times");
-			System.out.println("-------------------");
+			System.out.println("date elapsedtimes");
 		}
-}}
+	}
+}
