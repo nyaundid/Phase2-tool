7d87ef1af852406725be3d294c7c365e737316f0
seis-602-fall2017 <darius.nyaundi@LAPTOP-QG5H3RPR>
2017-12-09 20:01:38 -0600

    INITIAL CHECK IN FOR NEW FINAL

---
+++ NEW FOR FINAL/src/DATE.java
@@ -0,0 +1,16 @@
+public class DATE
+
+{
+
+	public static void main(String[] args)
+
+	{
+		java.util.Date date = new java.util.Date();
+		System.out.println(
+				" the dates and times for the elapsedtimes10000,100000,1000000,10000000,10000000,1000000000,1000000000,10000000000");
+		for (long i = 10000; i <= 1e11; i *= 10) {
+			date.setTime(i);
+			System.out.println(date.toString());
+		}
+	}
+}
