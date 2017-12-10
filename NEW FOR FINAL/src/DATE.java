efaff1de055931a77813897648c59e09bd76e526
seis-602-fall2017 <darius.nyaundi@LAPTOP-QG5H3RPR>
2017-12-09 22:20:43 -0600

    Add Caps and separation line from other dates

--- NEW FOR FINAL/src/DATE.java
+++ NEW FOR FINAL/src/DATE.java
@@ -7,11 +7,12 @@ public class DATE
 	{
 		java.util.Date date = new java.util.Date();
 		System.out.println(
-				" the dates and times for the elapsedtimes10000,100000,1000000,10000000,10000000,1000000000,1000000000,10000000000");
+				"The dates and times for the elapsedtimes 10000,100000,1000000,10000000,10000000,1000000000,1000000000,10000000000");
 		for (long i = 10000; i <= 1e11; i *= 10) {
 			date.setTime(i);
 			System.out.println(date.toString());
-			System.out.println("test");
+			System.out.println("Elapsed Times");
+			System.out.println("-------------------");
 		}
 	}
 }
